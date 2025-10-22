3f1b66a43eb3b111c549334d46bc9a0d8a582ca1
drproduck <dkp45@cornell.edu>
2025-10-21 22:03:57 -0400

    added

---
+++ scripts/train_sd15_5k_steps=100.sh
@@ -0,0 +1,16 @@
+accelerate launch --config-file "configs/train_multigpu.yaml" train_sd15_dpo_dr.py \
+    --mixed_precision "bf16" \
+    --pretrained_model_name_or_path "stable-diffusion-v1-5/stable-diffusion-v1-5" \
+    --output_dir "training_runs/sd15_5k" \
+    --labeled_manifest "datasets/manifest/from_5k/labeled.json" \
+    --unlabeled_manifest "datasets/manifest/from_5k/unlabeled.json" \
+    --train_data_dir "datasets/FiFA-100k/data/train" \
+    --train_batch_size 8 \
+    --gradient_accumulation_steps 8 \
+    --max_train_steps 100 \
+    --lr_warmup_steps 10 \
+    --learning_rate 1e-7 \
+    --checkpointing_steps 10000 \
+    --beta_dpo 5000 \
+    --gradient_checkpointing \
+    --report_to "wandb" \
