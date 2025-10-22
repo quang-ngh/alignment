7c139152ef2d3765a49dc2eb24a2c54a31b8d246
quang-ngh <quangngcs@gmail.com>
2025-10-21 23:51:11 -0400

    add script train

---
+++ scripts/train_sd15_dpo_base.sh
@@ -0,0 +1,17 @@
+accelerate launch --config-file "configs/train_multigpu.yaml" train_sd15_dpo_base.py \
+    --mixed_precision "bf16" \
+    --pretrained_model_name_or_path "./checkpoints/sd15" \
+    --output_dir "training_runs/sd15_dpo_base" \
+    --good_manifest "datasets/manifest/fifa_5k.json" \
+    --train_data_dir "datasets/FiFA-100k/data/train" \
+    --train_batch_size 8 \
+    --dataloader_num_workers 16 \
+    --gradient_accumulation_steps 8 \
+    --max_train_steps 1000 \
+    --learning_rate 1e-5 \
+    --num_train_epochs 10 \
+    --checkpointing_steps 100 \
+    --gradient_checkpointing \
+    --report_to "wandb" \
+    --tracker_project_name "sd15_dpo_base" \
+    --train_method "dpo" \
